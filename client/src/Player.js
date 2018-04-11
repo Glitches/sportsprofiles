5e261fa2b4442f4b1feddb1dffb153612343c08b
Andrea Ceccarelli <gltch2003@gmail.com>
2018-04-11 18:01:08 +0200

    Feature: add Semantic Ui and flexbox for players list

--- client/src/Player.js
+++ client/src/Player.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './Player.css';
+import {Card} from 'semantic-ui-react';
 
 class Player extends Component {
-
-    render() {
-
-	return(
-        <div className="player">
-            <div className="playerPhoto">
-            <img className="profilePhoto" width="150" alt={this.props.displayName} src={ "/img/" + this.props.alias + ".png" } />
-            </div>
-            <div className="playerName">
-            <NavLink to={'/player/' + this.props.id}>{this.props.displayName}</NavLink>
-            </div>
-            <div className="playerTeam">
-            {this.props.team}
-            </div>
+  render() {
+    return (
+      <div className="player">
+        <Card>
+          <div className="playerPhoto">
+            <img
+              className="profilePhoto"
+              width="150"
+              alt={this.props.displayName}
+              src={'/img/' + this.props.alias + '.png'}
+            />
+          </div>
+          <Card.Description>
+            <Card.Header className="playerName">
+              <NavLink to={'/player/' + this.props.id}>
+                {this.props.displayName}
+              </NavLink>
+            </Card.Header>
+            <div className="playerTeam">{this.props.team}</div>
             <div className="playerCombo">
-                #{this.props.teamJersey} | {this.props.teamShort} | {this.props.teamPos}
+              #{this.props.teamJersey} | {this.props.teamShort} |{' '}
+              {this.props.teamPos}
             </div>
-            <div className="playerCollege">
-                {this.props.college}
-            </div>
+            <div className="playerCollege">{this.props.college}</div>
             <div className="playerHW">
-            {this.props.height} {this.props.weight}
+              {this.props.height} {this.props.weight}
             </div>
-            <div className="playerBD">
-            {this.props.birthdate}
-            </div>
-            <div className="playerYears">
-            {this.props.yearspro} Years Pro
-            </div>
-        </div>
+            <div className="playerBD">{this.props.birthdate}</div>
+            <div className="playerYears">{this.props.yearspro} Years Pro</div>
+          </Card.Description>
+        </Card>
+      </div>
     );
-    }
+  }
 }
 
 export default Player;
--- client/src/Players.js
+++ client/src/Players.js
@@ -1,47 +1,42 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
+import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
-
+import './Players.css';
 import Player from './Player';
 
-const Players = ({ data: { loading, error, players }}) => {
-	if(loading) {
-		return <p>Loading...</p>
-	}
-	if(error) {
-		return <p>{error.message}</p>
-	}
-console.log(players);
-	return (
-		<div>
-		  { players.map(( item, index ) => 
-			<Player 
-				key={index} 
-				{...item}
-			/>
-			)}
-		</div>
-	  );
-}
+const Players = ({data: {loading, error, players}}) => {
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  console.log(players);
+  return (
+    <div className="PlayersList">
+      {players.map((item, index) => <Player key={index} {...item} />)}
+    </div>
+  );
+};
 
 export const playersQuery = gql`
   query PlayersQuery {
     players {
       id
       firstName
-	  	lastName
-	  	displayName
-			team
-			teamShort
-	  	teamJersey
-	  	teamPos
-			injuryTag
-			college
-			height
-			weight
-			birthdate
-			yearspro
-			alias
+      lastName
+      displayName
+      team
+      teamShort
+      teamJersey
+      teamPos
+      injuryTag
+      college
+      height
+      weight
+      birthdate
+      yearspro
+      alias
     }
   }
 `;
